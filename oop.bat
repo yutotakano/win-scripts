df4dfdb7a1cd4e9fa788f371a90427fbbbe9492a
Yuto Takano <moa17stock@gmail.com>
2021-04-12 04:00:50 +0900

    Create oop.bat

---
+++ oop.bat
@@ -0,0 +1,16 @@
+% Play the nth video in the INF1B-OOP youtube playlist with `oop n`
+@echo off
+set pos=%1
+if %1==0 goto wrong
+
+set /a pos=pos-1
+set _tail=%*
+call set _tail=%%_tail:*%1=%%
+mpv --playlist-start=%pos% --hwdec --script-opts=mpvcord-active=yes https://www.youtube.com/playlist?list=PLNFclWaZfiVKcXdPjMR9_-8Q4JwZdUxTX %_tail%
+goto exit
+
+:wrong
+echo Wrong arguments
+goto exit
+
+:exit
