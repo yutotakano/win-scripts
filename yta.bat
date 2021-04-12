add4f2f32be1fffcb72719dd010769d8ef8241e5
Yuto Takano <moa17stock@gmail.com>
2021-04-12 09:06:33 +0900

    Make it no longer get stuck in repeat loops

--- yta.bat
+++ yta.bat
@@ -1,12 +1,24 @@
-rem Start playing music with `yta search term here`
-rem Will keep autoplaying based on YouTube's autoplay thing
 @echo off
+setlocal EnableDelayedExpansion
 for /f "tokens=* usebackq" %%f in (`youtube-dl "ytsearch1:%*" --get-id`) do set playid=%%f
-set youtubeurl=https://www.youtube.com/watch?v=%playid%
+set currentUrl=https://www.youtube.com/watch?v=%playid%
+set /a videoCount=0
 
 :loop
-mpv --volume=50 --no-video --loop=no --loop-playlist=no --script-opts=mpvcord-active=yes %youtubeurl%
-for /F "usebackq tokens=*" %%i in (`curl -s "%youtubeurl%" ^| wsl grep -Po "\"autoplay\":{\"autoplay\":(?:.*?)\"watchEndpoint\":{\"videoId\":\"\K.*?(?^=\")" ^| wsl awk '{print "https://www.youtube.com/watch?v="$1}'`) do (
-set youtubeurl=%%i
-goto loop
+set history[!videoCount!]=%currentUrl%
+mpv --volume=50 --no-video --loop=no --loop-playlist=no --script-opts=mpvcord-active=yes %currentUrl%
+set found=false
+for /F "usebackq tokens=*" %%i in (`curl -s "%currentUrl%" ^| wsl grep -Po "\"watchEndpoint\":{\"videoId\":\"\K.*?(?^=\")" ^| wsl awk '{print "https://www.youtube.com/watch?v="$1}'`) do (
+    rem loop through all related videos and choose first one not in history
+    for /l %%h in (0,1,%videoCount%) do (
+        if !history[%%h]!==%%i (
+            set found=true
+        )
+    )
+    if !found!==false (
+        set /a videoCount=%videoCount%+1
+        set currentUrl=%%i
+        goto loop
+    )
 )
+echo Repetitive tracks reached, ending.
